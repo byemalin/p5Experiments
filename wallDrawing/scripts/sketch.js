8d7ad03ad0a2195ef51be244d88da7e41cc7688e
Malin Mabika <malinmabika@Malins-MacBook-Pro-16.local>
2023-11-24 11:02:45 +0100

    Fixing sketch.js linking path

---
+++ wallDrawing/scripts/sketch.js
@@ -0,0 +1,59 @@
+// TODO: Create setup() function
+function setup() {
+    // Inside the setup() function:
+    // TODO: Create canvas 500px wide and 500px high
+    createCanvas(500, 500);
+    // TODO: Draw horizontal and vertical guidelines
+    line(width/2, 0, width/2, height);
+};
+
+
+function draw() {
+    // TODO: Create wall drawing inside draw() function
+    
+      // Inside the draw() function:
+      // Draw parallel lines:
+      // TODO: Set stroke color and weight
+    
+    
+    
+      // TODO: Use a for loop to draw 10 vertical lines
+    
+    
+    
+      // Draw polka dots:
+      // TODO: Set no stroke and fill color
+    
+    
+    
+      // TODO: Use nested for loops to draw a grid of circles
+      // TODO: Offset y positions of every other column by 10 pixels
+    
+    
+    
+      // Draw checkered squares:
+      // TODO: Set fill color
+    
+    
+      // TODO: Use nested for loops to draw rows of squares
+      // TODO: Offset y positions of every other column by 25 pixels
+    
+    
+      
+      // Draw parallel diagonal lines:
+      // TODO: Set stroke color and weight
+    
+    
+      // TODO: Use a for loop to draw diagonal lines
+    
+    
+    
+      // Draw borders:
+      // TODO: Set stroke color
+    
+    
+      // TODO: Draw horizontal and vertical guidelines
+    
+    
+      // TODO: Draw borders around canvas
+}
