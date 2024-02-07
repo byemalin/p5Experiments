0bcb7797edb3973863c99b418e1f9360a3dffcbe
Malin Mabika <malinmabika@Malins-MacBook-Pro-16.local>
2024-02-07 14:43:49 +0200

    Simple Spiral Exercise

---
+++ simpleSpiral/scripts/sketch.js
@@ -0,0 +1,47 @@
+let yellowX, yellowY;
+let blueX, blueY;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  yellowX = width / 2;
+  yellowY = height / 2;
+
+  blueX = width / 2;
+  blueY = height / 2;
+
+  strokeWeight(4);
+}
+
+function draw() {
+  c = color('#201E1F');
+  c.setAlpha(10);
+  background(c);
+
+  // TODO: Add push and pop to isolate the yellow spiral
+  push();
+  translate(yellowX, yellowY); // Start of code for yellow spiral
+  rotate(frameCount * -0.002);
+  c2 = color('#A63D40');
+  c2.setAlpha(50);
+  stroke(c2);
+  for (let i = 0; i < 24; i++) {
+    rotate(PI / 12);
+    line(0, 0, 0, 240);
+  } // End of code for yellow spiral
+  pop();
+  
+  // TODO: Add push and pop to isolate the blue spiral
+  push();
+  translate(blueX, blueY); // Start of code for blue spiral
+  rotate(frameCount * 0.002 + PI / 8);
+  c3 = color('#E9B872');
+  c3.setAlpha(50);
+  stroke(c3);
+  for (let i = 0; i < 24; i++) {
+    rotate(PI / 12);
+    line(0, 0, 0, 240);
+  } // End of code for blue spiral
+  pop();
+
+}
